aaddc65c96792244e52b081aa64ba56ab323bf28
Sevvandi Kandanaarachchi <Sevvandi.Kandanaarachchi@monash.edu>
2019-02-11 16:53:46 +1100

    Add SVM functionality

--- R/Predict_Performance.R
+++ R/Predict_Performance.R
@@ -21,17 +21,17 @@ PredictPerformance <- function(ftrs, models){
 }
 
 #' Train models to predict outlier methods from meta-features of datasets.
-#' 
+#'
 #' @param d If \code{d=1} then we take the Min_Max performance values, if \code{d=2} then performance values from all normalization methods are considered. Input values for  \code{d} are only \code{1,2}.
 #' @param p If \code{p=1} then we take binary values based on absolute performance, i.e. if performance \code{ > 0.8}, if \code{p=2} the relative binary performance values are used. Input values for  \code{p} are only \code{1,2}.
 #' @param s If \code{s=1} then we train the models on a preferred subset of features. If \code{s=2} the models are trained on all features, which takes considerably longer.  Default value \code{s=1}. Input values for  \code{s} are only \code{1,2}.
-#' 
-#' @return  The trained randomforest models. 
-#'  
+#'
+#' @return  The trained randomforest models.
+#'
 #' @examples
 #' fit <- TrainModels(1,1,1)
-#' 
-#' 
+#'
+#'
 TrainModels <- function(d, p, s=1){
   # d is for meta-data set
   # d = 1 is the min_max performance data set
@@ -43,7 +43,7 @@ TrainModels <- function(d, p, s=1){
   # s is for subset of features or all features
   # s = 1 is for a subset of features
   # s = 2 is for all features
-  
+
   if((d!=1)&(d!=2)){
     stop("Invalid d. d should equal 1 or 2.")
   }
@@ -118,7 +118,7 @@ TrainModels <- function(d, p, s=1){
   mod_loop <- randomForest::randomForest(ftr_subset, as.factor(perfs[ ,10]) )
   mod_odin <- randomForest::randomForest(ftr_subset, as.factor(perfs[ ,11]) )
   mod_simlof <- randomForest::randomForest(ftr_subset, as.factor(perfs[ ,12]) )
-  
+
   models <- list()
   models$cof <- mod_cof
   models$fabod <- mod_fabod
@@ -141,12 +141,12 @@ TrainModels <- function(d, p, s=1){
 
 
 #' Cross validates models to reproduce results in  journal and conference papers.
-#' 
-#' @inheritParams TrainModels  
+#'
+#' @inheritParams TrainModels
 #' @param n The number of folds in cross validation.
 #'
 #' @return A list containing the following results from the cross validated models:
-#' \describe{   
+#' \describe{
 #'  \item{def_acc}{The default accuracy we get if we predict the method is not good for all instances. This is the percentage of the majority class.}
 #'  \item{results}{The \code{n}-fold cross valdation results. }
 #'  \item{mean_acc}{The mean \code{n}-fold cross valdation results.}
@@ -301,5 +301,162 @@ CrossValidateModels <- function(d, p, s=1, n=5){
   out$def_acc <- default_accuracy
   out$results <-result_table
   out$mean_acc <- apply(result_table, 2, mean)
+  return(out)
+}
+
+
+CrossValidateSVM <- function(d=1,n=5){
+  if((d!=1)&(d!=2)){
+    stop("Invalid d. d should equal 1 or 2.")
+  }
+
+  if(n > 10){
+    stop("Consider n less than or equal to 10.")
+  }
+
+  if(d==1){
+    # coordinates for min-max normalization
+    data("dat_4_svm_mm")
+    coordinates <- dat_4_svm_mm[, c(2,3)]
+    perfs <- dat_4_svm_mm[ ,4:15]
+    filenames <- dat_4_svm_mm[, 1]
+    cst <- 150
+    gmv <- 0.75
+  }else{
+    # d = 2
+    # coordinates for all normalization methods
+    # DO LATER!!
+  }
+
+  # Cross validation on file source as many variants of the same file exist
+  file_source <-c()
+  for(ll in 1:length(filenames)){
+    fname <- filenames[ll]
+    regobj1 <- regexpr("_C", fname)
+    regobj2 <- regexpr("_withoutdupl", fname)
+    if(regobj1[1]<0){
+      regobj <- regobj2
+    }else if(regobj2[1]<0){
+      regobj <- regobj1
+    }else{
+      regobj <- regobj1
+    }
+    end.ind <- regobj[1]-1
+    file_source <- c(file_source, substring(fname, 1, end.ind))
+  }
+  uniq_f_s <- unique(file_source)
+
+  # Create n equally size folds
+  set.seed(1234)
+  new_order <- sample(uniq_f_s,length(uniq_f_s))
+  folds <- cut(seq(1,length(uniq_f_s)),breaks=n,labels=FALSE)
+
+  cat("Starting", n, "fold cross validation. \n")
+
+  result_table <- matrix(0, nrow=n, ncol=dim(perfs)[2])
+
+  # Perform n fold cross validation
+  for(i in 1:n){
+    testSources <- new_order[which(folds==i,arr.ind=TRUE)]
+    testIndices <- which(file_source %in% testSources)
+
+    # Segement your data by fold
+    testData <- coordinates[testIndices, ]
+    trainData <- coordinates[-testIndices, ]
+    testLabels <- perfs[testIndices, ]
+    trainLabels <- perfs[-testIndices, ]
+    for(j in 1:dim(perfs)[2]){
+      cat("Fold ", i, " Method " , j, "... \n")
+      model <- e1071::svm(trainData, as.factor(trainLabels[ ,j]), cost = cst, gamma=gmv)
+      preds <- predict(model, testData, type="class")
+      result_table[i,j] <- sum(preds==testLabels[ ,j])/length(testLabels[ ,j])
+      print(paste("Accuracy",  result_table[i,j]) )
+    }
+  }
+  default_accuracy <- apply(perfs, 2, table)*100/dim(perfs)[1]
+  default_accuracy <- default_accuracy[1, ]
+  out <- list()
+  out$def_acc <- default_accuracy
+  out$results <-result_table
+  out$mean_acc <- apply(result_table, 2, mean)*100
+  return(out)
+}
+
+
+
+DrawInstSpace <- function(d=1){
+  if((d!=1)&(d!=2)){
+    stop("Invalid d. d should equal 1 or 2.")
+  }
+  if(d==1){
+    # Instance space using MIN_MAX
+    data("dat_4_svm_mm")
+    filenames <- dat_4_svm_mm[ ,1]
+    xx <- dat_4_svm_mm[, 2:3]
+    perfs <- dat_4_svm_mm[ , 4:15]
+    cst <- 150
+    gmv <- 0.75
+
+  }else{
+    # Instance space using all normalization methods
+    # FOR LATER!
+  }
+
+  # Train an SVM
+  preds.all <- matrix(0,nrow=nrow(xx), ncol=12)
+  preds.all.1.0 <- matrix(0,nrow=nrow(xx), ncol=12)
+  for(kk in 1:12){
+    dd2 <- cbind.data.frame(xx, as.factor(perfs[,kk]))
+    colnames(dd2) <- c("x", "y","Label" )
+
+    # Class weights - BEGIN
+    costs <- table(dd2$Label)
+    costs[1] <- 1
+    costs[2] <- c(1,round(table(dd2$Label)[1]/table(dd2$Label)[2]))[1]
+    # Class weights - END
+
+    mod.big <- e1071::svm(Label~.,  data=dd2, probability=TRUE, class.weights=costs, scale=FALSE, cost=cst, gamma=gmv )
+    pf.all <- predict(mod.big, dd2, probability=TRUE)
+    col <- which(colnames(attr(pf.all, "probabilities"))=="1")
+    preds.all[,kk] <- attr(pf.all, "probabilities")[,col]
+    preds.all.1.0[,kk] <- ifelse(preds.all[,kk] >=0.4,1,0)
+  }
+  colnames(preds.all.1.0)<-  colnames(perfs)
+
+
+  tt3 <- apply(preds.all.1.0,1,sum)
+  multiples <- which(tt3>1)
+  pp.multi <- apply(preds.all[multiples,],1,function(x) which.max(x))
+
+  pred.m <- rep(0,dim(preds.all.1.0)[1])
+  pred.m[multiples] <-pp.multi
+  qq <- apply(preds.all.1.0[-multiples,],1,function(x) ifelse(sum(x)==1,which(x==1), 0 ) )
+  pred.m[is.na(pred.m)] <- 0
+
+  pred.m[-multiples] <- qq
+  head(pred.m)
+  algorithms <- pred.m
+  algorithms[pred.m==0] <- "None"
+  algorithms[pred.m==1] <- "COF"
+  algorithms[pred.m==2] <- "FAST ABOD"
+  algorithms[pred.m==3] <- "INFLO" # "None" #
+  algorithms[pred.m==4] <- "KDEOS"
+  algorithms[pred.m==5] <- "KNN"
+  algorithms[pred.m==6] <- "KNNW"
+  algorithms[pred.m==7] <-  "LDF" # "None" #
+  algorithms[pred.m==8] <-  "LDOF" #
+  algorithms[pred.m==9] <-  "LOF" #
+  algorithms[pred.m==10] <-  "LOOP" #
+  algorithms[pred.m==11] <-  "ODIN" #
+  algorithms[pred.m==12] <-  "SIMLOF" #
+
+  print(ggplot2::ggplot(data=xx, aes(x,y))+ geom_point(aes(color=algorithms)))
+
+  out <- list()
+  out$preds10 <- preds.all.1.0
+  out$predprobs <- preds.all
+  out$algorithms <- algorithms
+  out$filenames <- filenames
+
   return(out)
 }
