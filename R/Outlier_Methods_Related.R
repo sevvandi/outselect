518ce77fa18519c40683d44de37674ca4a622877
Sevvandi Kandanaarachchi <Sevvandi.Kandanaarachchi@monash.edu>
2019-02-18 18:15:21 +1100

    add method CorrPrRoc

--- R/Outlier_Methods_Related.R
+++ R/Outlier_Methods_Related.R
@@ -24,11 +24,16 @@ ClusterMethods <- function( kk, rocpr=1, vis=FALSE){
   if(kk <2){
     stop("kk needs to be greater than 1.")
   }
+  if((rocpr!=1)&(rocpr!=2)){
+    stop("rocpr can only be 1 or 2.")
+  }
+  e <- new.env()
   if(rocpr==1){
-    data(perf_vals_roc_all)
+    data(perf_vals_roc_all, envir=e)
     dat <- perf_vals_roc_all[ ,2*1:28]
   }else{
-    stop("For later - for PR values")
+    data(perf_vals_pr_all, envir=e)
+    dat <- perf_vals_pr_all[ ,2*1:28]
   }
   cor_obj <- stats::cor(dat)
   dissimilarity <- 1 - cor_obj
@@ -38,4 +43,45 @@ ClusterMethods <- function( kk, rocpr=1, vis=FALSE){
     print(plot(pam(distance, k=kk)))
   }
   return(pam_obj)
+}
+
+#' Computes correlation between AUCROC performance values and AUCPR performance values
+#'
+#' @param m If \code{1}, then \code{pearson}, if \code{2}, then \code{kendall} or if \code{3}, then \code{spearman} correlation is performed. Default value is \code{1}.
+#'
+#' @return  The correlation values for each performance column.
+#'
+#' @examples
+#' \dontrun{
+#' corobj <- CorrPrRoc()
+#' }
+#'
+#' @export
+CorrPrRoc <- function(m=1){
+  if(m==1){
+    meth ="pearson"
+  }else if(m==2){
+    meth ="kendall"
+  }else if(m==3){
+    meth ="spearman"
+  }else{
+    stop("Invalid value for m. m needs to be 1, 2 or 3.")
+  }
+
+  e <- new.env()
+  data("features_all", envir=e)
+  data("features_all_pr",envir=e)
+  fnames_roc <- features_all[,1]
+  fnames_pr <- features_all_pr[,1]
+  roc_list <- which(fnames_roc %in% fnames_pr)
+  pr_list <- which(fnames_pr %in% fnames_roc)
+  data("perf_vals_pr_all", envir=e)
+  data("perf_vals_roc_all", envir=e)
+  cor_vals <- rep(0,56)
+  for(i in 1:56){
+    if(identical(colnames(perf_vals_roc_all)[i], colnames(perf_vals_pr_all)[i] ) ){
+      cor_vals[i] <- stats::cor(perf_vals_roc_all[roc_list,i], perf_vals_pr_all[pr_list,i], use="complete.obs", method=meth)
+    }
+  }
+  return(cor_vals)
 }
