2b397817d6dc96ea094df86e52dbdecebcd4a420
sevvandi <sevvandi.kandanaarachchi@monash.edu>
2019-03-11 20:42:48 +1100

    Norm method prediction

--- R/Norm_Prediction.R
+++ R/Norm_Prediction.R
@@ -137,7 +137,7 @@ CrossValidateSensitivityToNorm <- function(rocpr=1, xi=0.05, n=10){
 
 
 
-PredictNormMethod <- function(rocpr=1, xi=0.05, n=10){
+PredictNormMethod <- function(rocpr=1, xi=0.05, n=5, method_nums=1:14){
   # Predict normalization method, either Min_Max or Median_IQR
   if((rocpr!=1)&(rocpr!=2)){
     stop("Invalid rocpr. rocpr should equal 1 or 2.")
@@ -173,5 +173,66 @@ PredictNormMethod <- function(rocpr=1, xi=0.05, n=10){
   cat("Using all", length(col_nums), "features. This will take REALLY LONG!  \n")
 
 
+  methods <- c()
+  RF.accuracy.all <- matrix(0, nrow=n, ncol=length(method_nums))
+  default_acc <- rep(0, length(method_nums))
+  for(i in method_nums){
+    st <- 2*i-1
+    en <- st + 1
+
+    perfs.1 <- perfs[,st:en]
+    max.norm.1 <- apply(perfs.1,1,which.max)
+
+    pos <- regexpr("_", colnames(perfs.1)[1])[1] -1
+    method <- substring(colnames(perfs.1)[1], 1, pos)
+    methods <- c(methods, method)
+
+    ## find both good instances
+    sensitive.inds <- which(apply(perfs.1,1,max) - apply(perfs.1,1,min) >= xi)
+    feats <- feat[sensitive.inds,-1]
+    max.norm <- max.norm.1[sensitive.inds]
+    file_sources <- GetFileSources(filenames[sensitive.inds])
+    unique.sources <- unique(file_sources)
+
+    j <- which(method_nums==i)
+
+    default_accuracy <- table(max.norm.1)/length(max.norm.1)
+    default_acc[j] <- max(default_accuracy)
+
+
+    set.seed(123)
+    new.order <- sample(unique.sources,length(unique.sources))
+    #Create 5 equally size folds
+    folds <- cut(seq(1,length(unique.sources)),breaks=n,labels=FALSE)
+
+    for(kk in 1:n){
+      #Segement your data for n fold CV
+      testSources <- new.order[which(folds==kk,arr.ind=TRUE)]
+      testIndices <- which(file_sources %in% testSources)
+
+      testData <- as.matrix(feats[testIndices, ])
+      trainData <- as.matrix(feats[-testIndices, ])
+
+      y <- max.norm
+      testY <- y[testIndices]
+      trainY <- y[-testIndices]
+
+      ## random forest
+      rf.mod <- randomForest::randomForest(trainData, as.factor(trainY))
+      preds.rf <- predict(rf.mod,testData,type="class")
+
+
+      RF.accuracy.all[kk, j] <- sum(preds.rf==testY)/length(testY)
+
+    }
+
+  }
+
+  out <- list()
+  out$def_acc <- default_acc
+  out$results <-RF.accuracy.all
+  out$mean_acc <- apply(RF.accuracy.all, 2, mean)
+  out$methods <- methods
+  return(out)
 }
 
